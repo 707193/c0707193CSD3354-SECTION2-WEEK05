d4b107b5785e3f484dd335d87c4f347951f4fbc3
visualstudio <visualstudio@122-PC07>
2019-02-06 09:47:39 -0500

    9:38 AM COMMIT

--- c0707193/Program.cs
+++ c0707193/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0707193
+namespace _707193
 {
     class Program
     {
         static void Main(string[] args)
         {
+            MethodA();
+        }
+
+        public static void MethodA()
+        {
+            while (Peanut())
+            {
+                Console.WriteLine("help i am stuck in infinite loop");
+
+            }
+        }
+
+        public static bool Peanut()
+        {
+
+            // GGGG
+            return true;
         }
     }
 }
+
